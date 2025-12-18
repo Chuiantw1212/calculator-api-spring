f6b96e5e26800e4aa3b9eeb61e23fd221f162902
EN Chu <chuiantw1212@gmail.com>
2025-12-18 09:27:47 +0800

    add readme

--- src/main/java/com/en_chu/calculator_api_spring/constant/Gender.java
+++ src/main/java/com/en_chu/calculator_api_spring/constant/Gender.java
@@ -1,5 +1,13 @@
 package com.en_chu.calculator_api_spring.constant;
 
-public class Gender {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public enum Gender implements BaseEnum {
+	MALE("M", "男性"), FEMALE("F", "女性");
+
+	private final String code;
+	private final String description;
 }
